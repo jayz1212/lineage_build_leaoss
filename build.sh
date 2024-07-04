f2f331f3e86f559f126fe44175b645923e01e825
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-07-04 16:00:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -153,25 +153,31 @@ rm out/target/product/*/*.img
 
 #rm frameworks/base/packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
 #mv lineage_build_leaos/GlobalActionsDialog.java frameworks/base/packages/SystemUI/src/com/android/systemui/globalactions
+
+
+cd frameworks/base/
+git fetch https://github.com/LG-G6/android_frameworks_base.git lineage-18.1
+git cherry-pick f93240a9fe3e74e252e6e69db6993bee751a3515
+cd ../../
+
+# Charging animation
+cd frameworks/base/
+git cherry-pick 16be597d1893c3865a910d8926ab2a42424534c0
+cd ../../
+
+# PixelProps
+cd frameworks/base/
+git cherry-pick bdf722ffadd92ed4ef8b89fcd9e3b7338da1217b 315e6da47ee13856117ae0e7cb40fc8aa301e2bb 5a3d49d2801b6f216dda611ee5c0b5078c11e9ef c322ce30871d4b1ccde293c7e105db9ab4bef966 09d8dccc5f35543da84f4e03c26cf7091ca42788 2a6f604bbd30154a6175718c8ba6aa7af8262c17 fa90fd74a0812902353efdb730e3a50d02c97ebb cac54db135ee1e5ce7d889c407430f6711d05808 fa86c540bba6b4c225fb9cddf3e238339d10620d 3cf268ee6110f6d874fddaf67b11487ed464d833 1c12ea297b6ac699acfb772c578c1b932360c7ee 8467ca6b17d075aa4bd3fc742fa0ffdd17a85317 d1af265ed3442f594f62eaf25bfea896f5fab515 83fed2f6486a245302146a05406d346a18219750 011af8ff769aa910ea0df0d7c0f121a773e4279a
+cd ../../
+
+
     lunch ${TARGET}-userdebug
     make installclean
     make -j$(nproc --all) systemimage
 
 
-    for dir in out/target/product/*/; do
-  if [ -f "$dir/system.img" ]; then
-    mv "$dir/system.img" "system1.img"
-  fi
-done
 
 
-    cd frameworks/base
-sleep 1 &&git fetch https://github.com/xc112lg/android_frameworks_base.git patch-1
-sleep 1 &&git cherry-pick ff31cc43e514f3b57846d1cf221411fd08e9726e
-cd ../..
-
-       make installclean
-    make -j$(nproc --all) systemimage
 
    
     
